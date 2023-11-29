15c09ef6cd854120273d84d0e4871f139d6ac459
iamsk46 <lomalstekloy@gmail.com>
2023-11-29 18:32:29 +0300

    Ready

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[] a = new int[10];
+int count = 0;
+for (int i = 0; i < 10; i++)
+{
+Console.Write("a[{0}]=", i);
+a[i] = Convert.ToInt32(Console.ReadLine());
+if ((a[i] % 2) == 0)
+count++;
+}
+Console.Write(count);
+Console.ReadKey();
